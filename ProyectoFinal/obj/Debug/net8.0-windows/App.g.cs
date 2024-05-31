39830220a2a26fff395e0c665cfd7e0234088c53
ojuniormunguia <oscarmunguiasotelo@gmail.com>
2024-05-31 17:17:41 -0600

    asd

--- ProyectoFinal/obj/Debug/net8.0-windows/App.g.cs
+++ ProyectoFinal/obj/Debug/net8.0-windows/App.g.cs
@@ -47,7 +47,7 @@ namespace ProyectoFinal {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.2.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.5.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
@@ -73,7 +73,7 @@ namespace ProyectoFinal {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.2.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.5.0")]
         public static void Main() {
             ProyectoFinal.App app = new ProyectoFinal.App();
             app.InitializeComponent();
